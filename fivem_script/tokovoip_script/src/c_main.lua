12ee283a10a09618f5b7d423b94c3913fb69a137
Dylan Thuillier <itokoyamato@hotmail.fr>
2019-07-24 20:30:39 +0200

    chore: bump script to 1.3.4

--- fivem_script/tokovoip_script/src/c_main.lua
+++ fivem_script/tokovoip_script/src/c_main.lua
@@ -17,7 +17,7 @@
 local targetPed;
 local useLocalPed = true;
 local isRunning = false;
-local scriptVersion = "1.3.3";
+local scriptVersion = "1.3.4";
 local animStates = {}
 local displayingPluginScreen = false;
 local HeadBone = 0x796e;
