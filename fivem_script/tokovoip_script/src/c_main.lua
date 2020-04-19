ae615f92e1c1c9f7b0055f456e6c81c92efda23e
Poggu <45881722+Poggicek@users.noreply.github.com>
2020-04-19 19:29:54 +0200

    Fixed radios on onesync infinity

--- fivem_script/tokovoip_script/src/c_main.lua
+++ fivem_script/tokovoip_script/src/c_main.lua
@@ -118,28 +118,51 @@ local function clientProcessing()
 				tbl.volume = volume;
 				tbl.muted = 0;
 			end
-			--
-			-- Process channels
-			local remotePlayerUsingRadio = getPlayerData(playerServerId, "radio:talking");
-			local remotePlayerChannel = getPlayerData(playerServerId, "radio:channel");
 
-			for _, channel in pairs(voip.myChannels) do
-				if (channel.subscribers[voip.serverId] and channel.subscribers[playerServerId] and voip.myChannels[remotePlayerChannel] and remotePlayerUsingRadio) then
-					if (remotePlayerChannel <= voip.config.radioClickMaxChannel) then
-						tbl.radioEffect = true;
-					end
-					tbl.volume = 0;
-					tbl.muted = 0;
-					tbl.posX = 0;
-					tbl.posY = 0;
-					tbl.posZ = voip.plugin_data.enableStereoAudio and localPos.z or 0;
-				end
-			end
-			--
 			usersdata[#usersdata + 1] = tbl
 			setPlayerTalkingState(player, playerServerId);
 		end
 	end
+	
+	-- Process channels
+	for _, channel in pairs(voip.myChannels) do
+		for _, subscriber in pairs(channel.subscribers) do
+			if (subscriber ~= voip.serverId) then
+				local remotePlayerUsingRadio = getPlayerData(subscriber, "radio:talking");
+				local remotePlayerChannel = getPlayerData(subscriber, "radio:channel");
+				if(remotePlayerUsingRadio and remotePlayerChannel == channel.id) then
+					local remotePlayerUuid = getPlayerData(subscriber, "voip:pluginUUID");
+
+					local founduserData = nil
+					for k, v in pairs(usersdata) do
+						if(v.uuid == remotePlayerUuid) then
+							founduserData = v
+						end
+					end
+
+					if not founduserData then
+						founduserData = {
+							uuid = getPlayerData(subscriber, "voip:pluginUUID"),
+							resave = true
+						}
+					end
+
+					if (remotePlayerChannel <= voip.config.radioClickMaxChannel) then
+						founduserData.radioEffect = true;
+					end
+					founduserData.volume = 0;
+					founduserData.muted = 0;
+					founduserData.posX = 0;
+					founduserData.posY = 0;
+					founduserData.posZ = voip.plugin_data.enableStereoAudio and localPos.z or 0;
+					if(founduserData.resave) then
+						usersdata[#usersdata + 1] = founduserData
+					end
+				end
+			end
+		end
+	end
+
 	voip.plugin_data.Users = usersdata; -- Update TokoVoip's data
 	voip.plugin_data.posX = 0;
 	voip.plugin_data.posY = 0;
