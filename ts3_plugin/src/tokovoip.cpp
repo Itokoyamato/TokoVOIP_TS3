0aa4003496802cd6fa305cb4c687e30381efd74c
Dylan Thuillier <itokoyamato@hotmail.fr>
2020-06-21 23:29:21 +0200

    feat(plugin): remove unused code

--- ts3_plugin/src/tokovoip.cpp
+++ ts3_plugin/src/tokovoip.cpp
@@ -636,31 +636,6 @@ void Tokovoip::shutdown()
 	resetClientsAll();
 }
 
-vector<string> explode(const string& str, const char& ch) {
-	string next;
-	vector<string> result;
-
-	// For each character in the string
-	for (string::const_iterator it = str.begin(); it != str.end(); it++) {
-		// If we've hit the terminal character
-		if (*it == ch) {
-			// If we have some characters accumulated
-			if (!next.empty()) {
-				// Add them to the result vector
-				result.push_back(next);
-				next.clear();
-			}
-		}
-		else {
-			// Accumulate the next character into the sequence
-			next += *it;
-		}
-	}
-	if (!next.empty())
-		result.push_back(next);
-	return result;
-}
-
 // Utils
 
 bool isChannelWhitelisted(json data, string channel) {
