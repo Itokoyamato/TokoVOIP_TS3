760860ed81087e92b91858d07f5709a4a4236d8d
Itokoyamato <itokoyamato@hotmail.fr>
2020-07-06 21:16:00 +0200

    docs(ws-server): config.js better comments

--- ws_server/config.js
+++ ws_server/config.js
@@ -1,7 +1,19 @@
 module.exports = {
-  TSServer: "127.0.0.1", // IP Address of your teamspeak 3 server
-  // WSServerIP: "127.0.0.1", // IP Address of the ws-server is running on, if not set, will auto discover the current host public IP
-  WSServerPort: 3000, // Websocket port, default: 3000
-  // FivemServerIP: "127.0.0.1", // IP Adress of your FiveM server, if not set, will auto discover the current host public IP
-  // FivemServerPort: 32000, // Port of your FiveM Server, if not set, will try to contact FiveM server on same host
+  //-- [REQUIRED] IPv4 Address of your teamspeak 3 server
+  TSServer: "127.0.0.1",
+
+  //-- [REQUIRED] Port of the ws_server
+  WSServerPort: 3000,
+
+  //-- [OPTIONAL] IPv4 Address of the ws_server
+  //-- Set by autoconfig
+  // WSServerIP: "127.0.0.1",
+
+  //-- [OPTIONAL] IPv4 Adress of your FiveM server
+  //-- Set by autoconfig if you run ws_server as FXServer resource or standalone on the same machine
+  // FivemServerIP: "127.0.0.1",
+
+  //-- [OPTIONAL] Port of your FiveM Server
+  //-- Set by autoconfig if you run ws_server as FXServer resource
+  // FivemServerPort: 32000,
 };
