ba40e7bd0b0864c9926939c7761efa2232d99802
CryptoGenics <45253987+CryptoGenics@users.noreply.github.com>
2020-07-15 11:04:40 -0500

    rdr3 fxmanifest

--- fivem_script/tokovoip_script/fxmanifest.lua
+++ fivem_script/tokovoip_script/fxmanifest.lua
@@ -1,5 +1,6 @@
 fx_version 'bodacious'
-games { 'gta5' }
+games { 'gta5', 'rdr3' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 client_script "src/c_utils.lua"
 client_script "c_config.lua"
