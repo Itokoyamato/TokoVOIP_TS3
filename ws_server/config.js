ffe0760b9eed2d73444f86bf3f7fb4b3b43db55c
Itokoyamato <itokoyamato@hotmail.fr>
2020-07-26 23:55:49 +0200

    improv(ws-server): change default ws_server port

--- ws_server/config.js
+++ ws_server/config.js
@@ -3,8 +3,9 @@ module.exports = {
   TSServer: "127.0.0.1",
 
   //-- [REQUIRED] Port of the ws_server
-	//-- Make sure you open the port you specify below
-  WSServerPort: 3000,
+  //-- Make sure you open the port you specify below
+  //-- Please use a port above 30k as some networks block those below it
+  WSServerPort: 33250,
 
   //-- [OPTIONAL] IPv4 Address of the ws_server
   //-- Set by autoconfig
