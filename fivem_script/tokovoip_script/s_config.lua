88d9a813b5a00cc6ac8df1c5d4dbf9897f9e8248
Benjamin Durham <33912370+Bencey@users.noreply.github.com>
2020-07-27 12:23:00 +1200

    Explain what s_config is for

--- fivem_script/tokovoip_script/s_config.lua
+++ fivem_script/tokovoip_script/s_config.lua
@@ -1,7 +1,7 @@
 TokoVoipConfig = {
-	channels = {
-		{name = "PD Radio", subscribers = {}},
-		{name = "EMS Radio", subscribers = {}},
+	channels = { 
+		{name = "PD Radio", subscribers = {}}, -- Instead of TokoVoip HUD Saying "Talking on 1" It will say "Talking on PD Radio". The first line is for channel 1, second line is channel 2 etc 
+		{name = "EMS Radio", subscribers = {}}, 
 		{name = "PD/SO/EMS Shared Radio", subscribers = {}},
 		{name = "SO Radio", subscribers = {}},
 		{name = "PD/SO Shared Radio", subscribers = {}}
