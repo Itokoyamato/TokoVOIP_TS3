7070763b2bc5310a21c6a4dc60daecef0b70231c
Itokoyamato <itokoyamato@hotmail.fr>
2020-08-20 00:07:19 +0200

    chore(fivem-script): bump version to 1.5.5

--- fivem_script/tokovoip_script/src/c_main.lua
+++ fivem_script/tokovoip_script/src/c_main.lua
@@ -17,7 +17,7 @@
 local targetPed;
 local useLocalPed = true;
 local isRunning = false;
-local scriptVersion = "1.5.4";
+local scriptVersion = "1.5.5";
 local animStates = {}
 local displayingPluginScreen = false;
 local HeadBone = 0x796e;
